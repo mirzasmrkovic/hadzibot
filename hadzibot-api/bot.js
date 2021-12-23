026930b7244e083a865f541c01b3f42a0c1a7edf
Mirza Šmrković <mike@Mirzas-MacBook-Pro.local>
2021-12-23 12:22:56 +0100

    code refactoring & move bot to another group

--- hadzibot-api/bot.js
+++ hadzibot-api/bot.js
@@ -1,45 +1,114 @@
 const Telegraf = require('telegraf').Telegraf
+const Markup = require('telegraf').Markup
 const bot = new Telegraf(process.env.TG_BOT_TOKEN)
 const axios = require('axios')
 
-bot.command('open', async ctx => {
-  if (ctx.chat.id === parseInt(process.env.REZIJA_ID)) {
+const denyAccess = async (chatID, username) => {
+  Markup.removeKeyboard()
+
+  return await bot.telegram.sendMessage(
+    chatID,
+    `Denied access for *${username}*`,
+    { parse_mode: 'Markdown' }
+  )
+}
+
+bot.command('hadzibrava', async ctx => {
+  if (ctx.chat.id === parseInt(process.env.HADZIBRAVA_ID)) {
+    await ctx.deleteMessage()
+    return await bot.telegram.sendMessage(
+      ctx.chat.id,
+      `*${ctx.from.username}* is interacting with hadzibrava`,
+      {
+        parse_mode: 'Markdown',
+        ...Markup.keyboard([['Open HI', 'Close', 'Interfon'], ['Cancel']])
+          .oneTime(true)
+          .resize(),
+      }
+    )
+  } else {
+    await ctx.deleteMessage()
+    await denyAccess(ctx.chat.id, username)
+  }
+})
+
+bot.hears('Open HI', async ctx => {
+  const chatID = ctx.chat.id
+  const username = ctx.from.username
+
+  if (chatID === parseInt(process.env.HADZIBRAVA_ID)) {
+    await ctx.deleteMessage()
     await axios({
       method: 'post',
       url: process.env.API_URL + 'd=o',
     })
-    ctx.reply(`opened hi for ${ctx.from.username}`)
+    Markup.removeKeyboard()
+
+    return await bot.telegram.sendMessage(
+      chatID,
+      `opened hi for *${username}*`,
+      { parse_mode: 'Markdown' }
+    )
   } else {
-    ctx.reply(`denied access for ${ctx.from.username}`)
+    await ctx.deleteMessage()
+    await denyAccess(chatID, username)
   }
 })
 
-bot.command('close', async ctx => {
-  if (ctx.chat.id === parseInt(process.env.REZIJA_ID)) {
+bot.hears('Close', async ctx => {
+  const chatID = ctx.chat.id
+  const username = ctx.from.username
+
+  if (chatID === parseInt(process.env.HADZIBRAVA_ID)) {
+    await ctx.deleteMessage()
     await axios({
       method: 'post',
       url: process.env.API_URL + 'd=c',
     })
-    ctx.reply(`closed hi for ${ctx.from.username}`)
+    Markup.removeKeyboard()
+    return await bot.telegram.sendMessage(
+      chatID,
+      `closed hi for *${username}*`,
+      { parse_mode: 'Markdown' }
+    )
   } else {
-    ctx.reply(`denied access for ${ctx.from.username}`)
+    await ctx.deleteMessage()
+    await denyAccess(chatID, username)
   }
 })
 
-bot.command('interfon', async ctx => {
-  if (ctx.chat.id === parseInt(process.env.REZIJA_ID)) {
+bot.hears('Interfon', async ctx => {
+  const chatID = ctx.chat.id
+  const username = ctx.from.username
+
+  if (chatID === parseInt(process.env.HADZIBRAVA_ID)) {
+    await ctx.deleteMessage()
     await axios({
       method: 'post',
       url: process.env.API_URL + 'i=o',
     })
-    ctx.reply(`buzzed interfon for ${ctx.from.username}`)
+    Markup.removeKeyboard()
+    return await bot.telegram.sendMessage(
+      chatID,
+      `buzzed interfon for *${username}*`,
+      { parse_mode: 'Markdown' }
+    )
   } else {
-    ctx.reply(`denied access for ${ctx.from.username}`)
+    await ctx.deleteMessage()
+    await denyAccess(chatID, username)
   }
 })
 
-bot.command('ping', ctx => {
-  return ctx.replyWithMarkdown('*Pong!*')
+bot.hears('Cancel', async ctx => {
+  await ctx.deleteMessage()
+  Markup.removeKeyboard()
+})
+
+bot.hears('PING', async ctx => {
+  Markup.removeKeyboard()
+  return bot.telegram.sendMessage(ctx.chat.id, '*PONG!*', {
+    parse_mode: 'Markdown',
+  })
 })
 
 module.exports = bot
